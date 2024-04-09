0307d3628c1ca82cdc823eac7a6fcc661255db74
musekwa <musekwa2011@gmail.com>
2024-04-09 09:10:36 +0200

    fixed

--- src/app/dashboard/_components/columns.tsx
+++ src/app/dashboard/_components/columns.tsx
@@ -1,13 +1,28 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { Doc } from "../../../../convex/_generated/dataModel";
+import { Doc, Id } from "../../../../convex/_generated/dataModel";
 import { formatRelative } from "date-fns";
 import { useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import FileAction from "./file-actions";
 import { StarIcon } from "lucide-react";
+
+function UserCell({ userId }: { userId: Id<"users"> }) {
+  const userProfile = useQuery(api.users.getUserProfile, {
+    userId: userId,
+  });
+  return (
+    <div className="flex gap-2 text-xs text-gray-700 w-40 items-center">
+      <Avatar className="w-6 h-6">
+        <AvatarImage src={userProfile?.image} />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      {userProfile?.name}
+    </div>
+  );
+}
 
 export const columns: ColumnDef<Doc<"files"> & {
   isFavorited: boolean;
@@ -27,18 +42,7 @@ export const columns: ColumnDef<Doc<"files"> & {
     accessorKey: "user",
     header: "User",
     cell: ({ row }) => {
-      const userProfile = useQuery(api.users.getUserProfile, {
-        userId: row.original.userId,
-      });
-      return (
-        <div className="flex gap-2 items-center">
-          <Avatar className="w-6 h-6 ">
-            <AvatarImage src={userProfile?.image} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          {userProfile?.name}
-        </div>
-      );
+      return <UserCell userId={row.original.userId} />;
     },
   },
   {
